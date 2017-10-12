a251f7c7983b283f5f8fef69391adf10686b22c0
ghmajx <pzlmg@msn.com>
2017-10-12 14:26:51 +0800

    remove comments

--- amazon_jp_price.py
+++ amazon_jp_price.py
@@ -1,5 +1,6 @@
 #! python3
 import bs4, requests, sys, subprocess, time, logging, datetime, re
+
 logging.disable(logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 logging.debug('Start of program')
@@ -10,51 +11,6 @@ productid = sys.argv[1]
 url = 'https://www.amazon.co.jp/gp/product/' + productid
 print(url)
 
-# def getProdName(url):
-#     logging.debug('Start of getProdName')
-#     res = requests.get(url)
-#     amazonSoup = bs4.BeautifulSoup(res.text, "html.parser")
-#     amazonElem = amazonSoup.select('#productTitle')
-#     # logging.debug('The length of amazonElem is ' + str(len(jdElem)))
-#     prodName = amazonElem[0].getText().strip()
-#     logging.debug('product name is ' + prodName)
-#     return prodName
-#
-# def getprice(url):
-#     logging.debug('Start of getprice')
-#     res = requests.get(url)
-#     amazonSoup = bs4.BeautifulSoup(res.text, "html.parser")
-#     amazonElem = amazonSoup.select('#priceblock_ourprice')
-#     price = amazonElem[0].getText()
-#     priceDecimal = ''
-#     for i in price:
-#         if i.isdecimal():
-#             priceDecimal += i
-#
-#     return priceDecimal
-
-# def getProdInfo(url):
-#     logging.debug('Start of getProdInfo')
-#     prodInfo = {}
-#     res = requests.get(url)
-#     amazonSoup = bs4.BeautifulSoup(res.text, "html.parser")
-#     amzPriceElem = amazonSoup.select('#priceblock_ourprice')
-#     price = amzPriceElem[0].getText()
-#     priceDecimal = ''
-#     for i in price:
-#         if i.isdecimal():
-#             priceDecimal += i
-#
-#     prodInfo['price'] = priceDecimal
-#     logging.debug('product price is ' + prodInfo['price'])
-#     amazonProdElem = amazonSoup.select('#productTitle')
-#     # logging.debug('The length of amazonElem is ' + str(len(jdElem)))
-#     prodName = amazonProdElem[0].getText().strip()
-#
-#     prodInfo['prodName'] = prodName
-#     logging.debug('product name is ' + prodInfo['prodName'])
-#     # logging.debug('prodInfo is ' + prodInfo.items())
-#     return prodInfo
 
 def getProdInfo(url):
     logging.debug('Start of getProdInfo')
@@ -74,23 +30,17 @@ def getProdInfo(url):
 
     prodInfo['prodName'] = prodName
     logging.debug('product name is ' + prodInfo['prodName'])
-    # logging.debug('prodInfo is ' + prodInfo.items())
     return prodInfo
+
 
 while True:
     try:
-        # print(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - '+ getProdInfo(url)['prodName'] + ' - ' + getProdInfo(url)['price'])
         productInfo = getProdInfo(url)
         price = productInfo['price']
         prodName = productInfo['prodName']
-        msg = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - '+ prodName + ' - ' + price
+        msg = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - ' + prodName + ' - ' + price
         logging.debug("The msg is " + msg)
         if int(price) < 440:
-            # alarmFile = open('d:\\amazon\\alarmFile.txt', 'w')
-            # alarmFile.write(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - '+ getProdName(url) + ' - ' + getprice(url))
-            # alarmFile.close()
-            # subprocess.Popen(['start', 'd:\\amazon\\alarmFile.txt'], shell=True)
-            # subprocess.Popen(['start', 'd:\\amazon\\Sent.wav'], shell=True)
             requests.post('https://api.telegram.org/botbid/sendMessage?chat_id=uid&text=' + msg
                           + ' - ' + url)
         time.sleep(15)
--- jd_price.py
+++ jd_price.py
@@ -1,6 +1,7 @@
 #! python3
 import bs4, requests, sys, subprocess, time, logging, datetime, json
-#logging.disable(logging.DEBUG)
+
+logging.disable(logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 logging.debug('Start of program')
 if len(sys.argv) < 2:
@@ -9,19 +10,18 @@ if len(sys.argv) < 2:
 productid = sys.argv[1]
 nameUrl = 'https://item.jd.com/' + productid + '.html'
 priceUrl = 'https://p.3.cn/prices/mgets?skuIds=J_' + productid
-logging.debug('The name url is ' + nameUrl +'\n' +
+logging.debug('The name url is ' + nameUrl + '\n' +
               'The price url is ' + priceUrl)
+
 
 def getProdName(url):
     logging.debug('Start of getProdName')
     res = requests.get(url)
     jdSoup = bs4.BeautifulSoup(res.text, "html.parser")
     jdElem = jdSoup.select('.sku-name')
-    # logging.debug('The length of jdElem is ' + str(len(jdElem)))
     prodName = jdElem[0].getText().strip()
     logging.debug('product name is ' + prodName)
     return prodName
-
 
 
 def getprice(url):
@@ -29,30 +29,16 @@ def getprice(url):
     res = requests.get(url)
     price = json.loads(res.text)[0]['p']
     logging.debug('price is' + price)
-    # priceDecimal = ''
-    # for i in price:
-    #     if i.isdecimal():
-    #         priceDecimal += i
-    #
-    # return priceDecimal
     return price
-# try:
-#     print(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - '+ getProdName(nameUrl) + ' - ' + getprice(priceUrl))
-# except Exception as e:
-#     print(e)
+
 
 while True:
     try:
         price = getprice(priceUrl)
         prodName = getProdName(nameUrl)
-        msg = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - '+ prodName + ' - ' + price
+        msg = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - ' + prodName + ' - ' + price
         logging.debug('The msg is ' + msg)
         if float(price) < 8000:
-            # alarmFile = open('d:\\amazon\\alarmFile.txt', 'w')
-            # alarmFile.write(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - '+ getProdName(nameUrl) + ' - ' + getprice(priceUrl))
-            # alarmFile.close()
-            # subprocess.Popen(['start', 'd:\\amazon\\alarmFile.txt'], shell=True)
-            # subprocess.Popen(['start', 'd:\\amazon\\Sent.wav'], shell=True)
             requests.post(
                 'https://api.telegram.org/botbid/sendMessage?chat_id=uid&text=' + msg
                 + ' - ' + nameUrl)
