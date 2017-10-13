c169df149679c075bb368e975b26c626d0413013
ghmajx <pzlmg@msn.com>
2017-10-13 17:34:09 +0800

    add amazon.com

---
+++ amazon_price.py
@@ -0,0 +1,50 @@
+#! python3
+import bs4, requests, sys, subprocess, time, logging, datetime, re
+
+logging.disable(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of program')
+if len(sys.argv) < 2:
+    print('Usage: python amazon_jp_price.py [productid] - get productid price')
+    sys.exit()
+productid = sys.argv[1]
+url = 'https://www.amazon.com/gp/product/' + productid
+print(url)
+
+
+def getProdInfo(url):
+    logging.debug('Start of getProdInfo')
+    prodInfo = {}
+    res = requests.get(url)
+    amazonSoup = bs4.BeautifulSoup(res.text, "html.parser")
+    amzElem = amazonSoup.find_all(id=re.compile(r'productTitle$|priceblock_ourprice$'))
+    # Version 0.3:
+    priceRaw = amzElem[1].getText()
+    priceRegex = re.compile(r'\d+\.?\d*')
+    mo = priceRegex.search(priceRaw)
+    price = mo.group()
+    logging.debug('After Regex the price is ' + price)
+
+    prodInfo['price'] = price
+    logging.debug('product price is ' + prodInfo['price'])
+    prodName = amzElem[0].getText().strip()
+
+    prodInfo['prodName'] = prodName
+    logging.debug('product name is ' + prodInfo['prodName'])
+    return prodInfo
+
+
+while True:
+    try:
+        productInfo = getProdInfo(url)
+        price = productInfo['price']
+        prodName = productInfo['prodName']
+        msg = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + ' - ' + prodName + ' - ' + price
+        logging.debug("The msg is " + msg)
+        if int(price) < 440:
+            requests.post('https://api.telegram.org/botbid/sendMessage?chat_id=uid&text=' + msg
+                          + ' - ' + url)
+        time.sleep(15)
+    except Exception as e:
+        print(e)
+        continue
